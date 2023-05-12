db0e40c44203b7dca0e4ef155c9900cf908a349c
Gihyun <giordnnuz@gmail.com>
2023-05-12 14:46:41 +0800

    Added restart button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,9 @@
 	import '../app.css';
 </script>
 
-<main class="h-screen flex items-center">
+<nav class="h-20 flex items-center justify-center p-4 border-b-[1px] border-b-neutral-700">
+  <h1 class="text-white text-3xl text-center">Woordle</h1>
+</nav>
+<main class="flex items-center flex-col pt-40">
   <slot />
 </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,8 @@
 
 	let currentRow = 0;
 	let currentCol = 0;
+	let isWinner = false;
+	let isGameOver = false;
 
 	function keyboardEvents() {
 		document.body.onkeydown = (e) => {
@@ -90,16 +92,47 @@
 			box.style.animationDelay = `${(i * animationDuration) / 2}ms`;
 		}
 
-		const isWinner = secretWord === guess;
-		const isGameOver = currentRow === 6;
+		isGameOver = currentRow === 6;
 
 		setTimeout(() => {
-			if (isWinner) {
-				alert('Congratulations!');
+			if (secretWord === guess) {
+				isWinner = true;
 			} else if (isGameOver) {
 				alert(`Nice try! The word was ${secretWord}.`);
 			}
 		}, 3 * animationDuration);
+	}
+
+	function reset() {
+		secretWord = words[Math.floor(Math.random() * words.length)];
+		matrix = Array(6)
+			.fill('')
+			.map(() => Array(6).fill(''));
+
+		currentRow = 0;
+		currentCol = 0;
+		isWinner = false;
+		isGameOver = false;
+
+		// Might be inefficient but who cares
+		const box = document.querySelectorAll(`[data-num]`) as NodeListOf<HTMLDivElement>;
+
+		box.forEach((box) => {
+			if (box.classList.contains('bg-green-500')) {
+				box.classList.remove('bg-green-500');
+			}
+			if (box.classList.contains('bg-yellow-500')) {
+				box.classList.remove('bg-yellow-500');
+			}
+			if (box.classList.contains('bg-neutral-600')) {
+				box.classList.remove('bg-neutral-600');
+			}
+			if (box.classList.contains('animate-flip')) {
+				box.classList.remove('animate-flip');
+			}
+
+			box.style.animationDelay = '';
+		});
 	}
 
 	onMount(() => {
@@ -112,11 +145,17 @@
 	{#each matrix as rows, rowIdx (rowIdx)}
 		{#each rows as letter, letterIdx (letterIdx)}
 			<div
-				class="w-16 h-16 border-2 border-white uppercase text-white text-4xl grid place-items-center"
+				class="w-16 h-16 border-2 border-neutral-500 uppercase text-white text-4xl grid place-items-center select-none"
 				data-num={`${rowIdx}-${letterIdx}`}
 			>
 				{letter}
 			</div>
 		{/each}
 	{/each}
+</div>
+<div class={`my-10 flex flex-col items-center ${isWinner ? 'visible' : 'invisible'}`}>
+	<p class="text-white text-3xl mb-5">Great job!</p>
+	<button class="text-black bg-white text-2xl px-4 py-2 rounded-lg max-w-fit" on:click={reset}
+		>Restart</button
+	>
 </div>
